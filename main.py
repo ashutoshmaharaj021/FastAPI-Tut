6ffed109b605b19ca63c251aa1fb7e3954d27d39
ashutoshmaharaj021 <ashutoshmaharaj432@gmail.com>
2026-09-20 11:31:43 +0530

    Defined Paths Parameters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -20,3 +20,14 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve",example="P001")):
+    #load all patients
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        return {'error': 'Patient not found'}
+
+
